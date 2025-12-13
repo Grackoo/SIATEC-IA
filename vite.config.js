8aa41cfb131f9ed15b6db3459bed9b609793d3d6
Grackoo <jonnathanjosue@outlook.com>
2025-12-12 22:49:34 -0600

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Añadimos la propiedad 'base' y la configuramos como './' (ruta relativa).
+  // Esto fuerza a Vite a generar rutas relativas para todos los assets (JS, CSS),
+  // asegurando que se carguen correctamente sin importar si la aplicación
+  // se aloja en la raíz o en un subdirectorio (como en GitHub Pages).
+  base: './',
   plugins: [react()],
 })
